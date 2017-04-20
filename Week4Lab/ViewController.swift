e7f88fe56433f60c54cf2d650b3599c9d874c091
Danny Glover <danny@plastiq.com>
2017-04-19 19:13:33 -0700

    Initial Commit

---
+++ Week4Lab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week4Lab
+//
+//  Created by Danny Glover on 4/19/17.
+//  Copyright © 2017 Danny Glover. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
